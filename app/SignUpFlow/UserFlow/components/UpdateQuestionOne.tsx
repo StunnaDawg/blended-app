fa70546093d92eb3b5dcb9b494e0cf2e390950eb
StunnaDawg <jonsonallen9@gmail.com>
2024-02-26 10:22:18 -0400

    expo location

--- app/SignUpFlow/UserFlow/components/UpdateQuestionOne.tsx
+++ app/SignUpFlow/UserFlow/components/UpdateQuestionOne.tsx
@@ -1,9 +1,10 @@
 import { View, Text, Button } from "react-native"
-import React from "react"
+import React, { useState } from "react"
 import { NavigationType } from "../../../@types/navigation"
 import { useNavigation } from "@react-navigation/native"
 import { doc, updateDoc } from "firebase/firestore"
 import { FIREBASE_AUTH, db } from "../../../../firebase"
+import * as Location from "expo-location"
 
 type QuestionOneProps = {
   id?: string
@@ -20,6 +21,8 @@ const UpdateQuestionOne = ({
   gender,
   intentions,
 }: QuestionOneProps) => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+  const [errorMsg, setErrorMsg] = useState<string | null>(null)
   const navigation = useNavigation<NavigationType>()
   const submitUserQuestions = async () => {
     try {
@@ -40,11 +43,29 @@ const UpdateQuestionOne = ({
     }
   }
 
+  const enableLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync()
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied")
+      return
+    }
+
+    let location = await Location.getCurrentPositionAsync({})
+    setLocation(location)
+    let text = "Waiting.."
+    if (errorMsg) {
+      text = errorMsg
+    } else if (location) {
+      text = JSON.stringify(location)
+    }
+  }
+
   return (
     <Button
       title="Next"
       onPress={async () => {
         await submitUserQuestions()
+        await enableLocation()
         navigation.navigate("UserQuestionTwo")
       }}
     />
