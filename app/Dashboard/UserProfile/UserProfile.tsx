db526bd9041f86b12f74e1251cd6e1a218eddb2c
StunnaDawg <jonsonallen9@gmail.com>
2024-02-26 12:23:37 -0400

    city location for user

--- app/Dashboard/UserProfile/UserProfile.tsx
+++ app/Dashboard/UserProfile/UserProfile.tsx
@@ -3,15 +3,28 @@ import React, { useEffect, useState } from "react"
 import ProfilePic from "../../components/Avatar"
 import { FIREBASE_AUTH, db } from "../../../firebase"
 import { doc, getDoc } from "firebase/firestore"
-import { id } from "date-fns/locale"
 import { useNavigation } from "@react-navigation/native"
 import { NavigationType } from "../../@types/navigation"
+import * as Location from "expo-location"
 
 const UserProfile = () => {
   const [name, setName] = useState<string>("")
   const [age, setAge] = useState<string>("")
+  const [location, setLocation] = useState<Location.LocationObject | null>(null)
+  const [city, setCity] = useState<string | null>(null)
   const navigation = useNavigation<NavigationType>()
   const currentId = FIREBASE_AUTH.currentUser?.uid
+  useEffect(() => {
+    getUserNameAge()
+  }, [])
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+  useEffect(() => {
+    getCity()
+  }, [])
 
   const getUserNameAge = async () => {
     if (currentId) {
@@ -27,10 +40,32 @@ const UserProfile = () => {
       }
     }
   }
+  const getLocation = async () => {
+    try {
+      let currentLocation = await Location.getCurrentPositionAsync({})
+      setLocation(currentLocation)
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
-  useEffect(() => {
-    getUserNameAge()
-  }, [])
+  const getCity = async () => {
+    try {
+      if (location) {
+        let currentCity = await Location.reverseGeocodeAsync({
+          latitude: location?.coords.latitude,
+          longitude: location?.coords.latitude,
+        })
+        if (currentCity.length > 0) {
+          // Extract the city from the address
+          setCity(currentCity[0].city)
+        }
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <View>
       <ProfilePic
@@ -43,7 +78,7 @@ const UserProfile = () => {
         photoType="userPhotos"
       />
       <Text>
-        {name}, {age}
+        {name}, {city ? city : "no location"}
       </Text>
       <Button
         title="edit profile"
