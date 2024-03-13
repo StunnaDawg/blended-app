3b8fb5ebae7e3690d9165a9b7447a2c3498a3656
StunnaDawg <jonsonallen9@gmail.com>
2024-03-12 21:10:27 -0300

    first iteration of create event

--- app/gymSide/Events/CreateEvent.tsx
+++ app/gymSide/Events/CreateEvent.tsx
@@ -1,33 +1,82 @@
-import { View, Text, TextInput } from "react-native"
+import { View, Text, TextInput, Pressable } from "react-native"
 import { Event } from "../../@types/firestore"
 import React, { useState } from "react"
 import UploadImage from "../../components/UploadImage"
-import UpdatePhotosUser from "../../SignUpFlow/UserFlow/components/UpdatePhotosUser"
-import UpdateEventPhotosProps from "./components/UpdatePhotosUser"
-import { FIREBASE_AUTH } from "../../../firebase"
+import { FIREBASE_AUTH, db } from "../../../firebase"
+import { ScrollView } from "react-native"
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  updateDoc,
+} from "firebase/firestore"
 
 const CreateEvent = () => {
   const [description, setDescription] = useState<string>("")
+  const [eventTitle, setEventTitle] = useState<string>("")
   const [date, setDate] = useState<Date>()
   const [location, setLocation] = useState<string>()
-  const [price, setPrice] = useState<number>(0)
+  const [price, setPrice] = useState<string>("")
   const [eventPictures, setEventPictures] = useState<string[]>([])
 
   const currentGymId = FIREBASE_AUTH.currentUser?.uid
+  const submitUserPhotos = async (image: string[]) => {
+    try {
+      if (currentGymId) {
+        const gymRef = doc(db, "gyms", currentGymId, "events")
+
+        await updateDoc(gymRef, {
+          eventPictures: arrayUnion(image),
+        })
+      } else {
+        console.log("Gym does not exist")
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  const createEvent = async () => {
+    try {
+      if (currentGymId) {
+        const gymRef = collection(db, "gyms", currentGymId, "events")
+        await addDoc(gymRef, {
+          gymHost: currentGymId,
+          eventTitle: eventTitle,
+          description: description,
+          location: location,
+          date: date,
+          price: price,
+        })
+      } else {
+        console.log("Gym does not exist")
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
 
   return (
-    <View>
+    <ScrollView>
       <View className="flex flex-row justify-center">
         <Text className="text-3xl font-semibold">Create Event</Text>
       </View>
 
       <View>
         <Text>Title</Text>
-        <TextInput placeholder="Add a Title" />
+        <TextInput
+          value={eventTitle}
+          onChangeText={(title) => setEventTitle(title)}
+          placeholder="Add a Title"
+        />
       </View>
       <View>
         <Text>Description</Text>
-        <TextInput placeholder="Add a Title" />
+        <TextInput
+          value={description}
+          onChangeText={(description) => setDescription(description)}
+          placeholder="Add a Title"
+        />
       </View>
       <View>
         <Text>When?</Text>
@@ -35,18 +84,33 @@ const CreateEvent = () => {
       </View>
       <View>
         <Text>Price?</Text>
-        <TextInput placeholder="Add a Title" />
+        <TextInput
+          value={price}
+          onChangeText={(price) => setPrice(price)}
+          placeholder="Add a Title"
+        />
       </View>
       <View>
         <Text>Where?</Text>
-        <TextInput placeholder="Add a Title" />
+        <TextInput
+          value={location}
+          onChangeText={(location) => setLocation(location)}
+          placeholder="Add a Title"
+        />
       </View>
       <View>
         <UploadImage setUris={setEventPictures} uris={eventPictures} />
-
-        <UpdateEventPhotosProps id={currentGymId} imageArray={eventPictures} />
       </View>
-    </View>
+
+      <Pressable
+        onPress={() => {
+          createEvent()
+          submitUserPhotos(eventPictures)
+        }}
+      >
+        <Text className="font-bold text-xl">Create Event</Text>
+      </Pressable>
+    </ScrollView>
   )
 }
 
